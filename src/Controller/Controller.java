f56d1e30da8a8ba0298f80ebadec2f9135eb7522
Luís <88602145+luiscff@users.noreply.github.com>
2024-05-11 11:19:38 +0100

    Add feature: Main Menu

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -11,10 +11,14 @@ import java.io.IOException;
 public class Controller {
     //esta classe vai ser responsável por controlar o fluxo do programa
     //vai receber ‘input’ do utilizador a partir da View, chamar métodos do Model e devolver ‘output’ para a View
-    private final Model model;
+    private Model model;
     private final View view;
     private final PersistenceManager persistenceManager;
-
+    private final String[] menuInicial = {"Carregar estado do programa", "Registar Utilizador", "Iniciar Sessão",
+            "Avançar no tempo", "Estatísticas", "Guardar estado do programa"};
+    private final String[] menuUtilizador = {"Consultar Atividades", "Consultar Plano de Treino",
+            "Registar nova Atividade", "Editar Atividade", "Editar Plano de Treino", "Editar Perfil",
+            "Guardar estado do programa"};
 
     public Controller(Model model, View view) {
         this.model = model;
@@ -24,32 +28,66 @@ public class Controller {
 
     public void run() {
         //este método vai ser chamado no método init() da classe Workout_APP
+        //vai ser responsável por controlar o fluxo do programa
 
 
-        try {
-            persistenceManager.saveState("state.bin", model);
-        } catch (IOException e) {
-            view.printError(e, "Erro ao guardar o estado do programa");
+//        try {
+//            persistenceManager.saveState("state.bin", model);
+//        } catch (IOException e) {
+//            view.printError(e, "Erro ao guardar o estado do programa");
+//        }
+//
+//        try {
+//            Model model = persistenceManager.loadState("state.bin");
+//        } catch (IOException | ClassNotFoundException e) {
+//            view.printError(e, "Erro ao carregar o estado do programa");
+//        }
+
+        while (true){
+            view.printMenu(menuInicial);
+            int option = view.readMenuOption(menuInicial.length);
+            switch (option) {
+                case 0: //sair
+                    System.exit(0);
+                    break;
+                case 1: //carregar estado do programa
+                    String fileName = view.readFileName();
+                    try {
+                        model = persistenceManager.loadState(fileName);
+                        view.printMessage("Estado do programa carregado com sucesso");
+                    } catch (IOException | ClassNotFoundException e) {
+                        view.printError(e, "Erro ao carregar o estado do programa");
+                    }
+                    break;
+                case 2:
+//                    view.registarUtilizador();
+//                    model.registarUtilizador();
+                    view.printMessage("Feature ainda não implementada");
+                    break;
+                case 3:
+//                    view.iniciarSessao();
+//                    this.runUser();
+                    view.printMessage("Feature ainda não implementada");
+                    break;
+                case 4:
+                    //avançar no tempo
+                    view.printMessage("Feature ainda não implementada");
+                    break;
+                case 5:
+                    //estatísticas
+                    view.printMessage("Feature ainda não implementada");
+                    break;
+                case 6: //guardar estado do programa
+                    String fileName2 = view.readFileName();
+                    try {
+                        persistenceManager.saveState(fileName2, model);
+                        view.printMessage("Estado do programa guardado com sucesso");
+                    } catch (IOException e) {
+                        view.printError(e, "Erro ao guardar o estado do programa");
+                    }
+                    break;
+            }
         }
 
-        try {
-            Model model = persistenceManager.loadState("state.bin");
-        } catch (IOException | ClassNotFoundException e) {
-            view.printError(e, "Erro ao carregar o estado do programa");
-        }
-
-
-
-//        view.imprimirMenuInial();
-//        int opcao = view.lerOpcao();
-//        switch (opcao) {
-//            case 1:
-//                persistenceManager.save(model);
-//                break;
-//            case 2:
-//                model = persistenceManager.load();
-//                break;
-//            case 3:
-//                          etc...
     }
 }
--- src/View/View.java
+++ src/View/View.java
@@ -1,10 +1,70 @@
 package View;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class View {
+
+    Scanner sc;
+    Logger logger = Logger.getLogger(View.class.getName());
+
+    public View() {
+        sc = new Scanner(System.in);
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
 
     public void printError(Exception e, String message) {
         System.out.println(message);
-        e.printStackTrace();
+        logger.severe(e.getMessage());
+    }
+
+    public void printMenu(String[] menu) {
+        System.out.println();
+        System.out.println("MENU:");
+        int longest_option = 0;
+        for (int i = 0; i < menu.length; i++) { //prints each option
+            if (menu[i].length() > longest_option) { //updates longest_option
+                longest_option = menu[i].length();
+            }
+            System.out.print((i+1) + ". ");
+            System.out.println(menu[i]);
+        }
+        System.out.println(0 + ". Sair");
+        for (int i = 0; i < longest_option; i++) System.out.print("-"); //prints a line of dashes
+        System.out.println();
+    }
+
+    public int readMenuOption(int options_count) {
+        boolean valid_option = false;
+        int option = 0; // opção ‘default’ é o sair
+        while (!valid_option){
+            System.out.print("Enter an option: ");
+            try {
+                option = sc.nextInt();
+                if (option < 0 || option > options_count) {
+                    throw new Exception("Invalid option");
+                }
+                valid_option = true;
+            } catch (Exception e) {
+                System.out.println("Please enter a valid option");
+            }
+        }
+        return option;
+
+    }
+
+    public String readFileName() {
+        System.out.print("Insere o nome do Ficheiro: ");
+        String fileName = sc.next();
+        while (!fileName.endsWith(".bin")) {
+            System.out.println("Nome de ficheiro tem de terminar em '.bin'");
+            System.out.print("Insere o nome do Ficheiro: ");
+            fileName = sc.next();
+        }
+        return fileName;
     }
 
 }
